1241a5b48b3b4cc383887f16ac2ea43617c63737
Patryk Brochocki <rudyjestrudy0000@gmail.com>
2025-03-03 17:05:33 +0100

    Update Zajecia2.sql

--- Zajecia2.sql
+++ Zajecia2.sql
@@ -3,3 +3,13 @@ s24.ABCD
 mysql -p
 s24.ABCD
 use s24Patryk
+
+Konkatanacja - łączenie stringów
+
+
+select concat(nazwisko, imie) from osoby;
+select id, concat(nazwisko, " ", imie), pesel from osoby;
+
+select concat("Samochód ", marka," kosztuje ", cena, "zł.") from auta;
+select concat(marka, " kosztuje ", cena, "zł.") as "Cennik" from auta;
+
